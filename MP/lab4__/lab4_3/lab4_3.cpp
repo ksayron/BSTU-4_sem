af9209f21123a79044be89dd7cbe721c0c72dff2
KuchN <mrkola45@gmail.com>
2025-03-22 17:50:14 +0300

    added MP 4 lab

---
+++ MP/lab4__/lab4_3/lab4_3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "LCS.h"
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    char X[] = "ALDC", Y[] = "LADCM";
+    std::cout << std::endl << "-- вычисление длины LCS для X и Y(рекурсия)";
+    std::cout << std::endl << "-- последовательность X: " << X;
+    std::cout << std::endl << "-- последовательность Y: " << Y;
+    int s = lcs(
+        sizeof(X) - 1,  // длина   последовательности  X   
+        "ALDC",       // последовательность X
+        sizeof(Y) - 1,  // длина   последовательности  Y
+        "LADCM"       // последовательность Y
+    );
+    std::cout << std::endl << "-- длина LCS: " << s << std::endl;
+    system("pause");
+    return 0;
+}
---
+++ MP/lab4__/lab4_4/lab4_4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "LCS.h"    
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    char z[100] = "";
+    char x[] = "ABCBDAB",
+        y[] = "BDCABA";
+
+    int l = lcsd(x, y, z);
+    std::cout << std::endl
+        << "-- наибольшая общая подпоследовательость - LCS(динамическое"
+        << "программирование)" << std::endl;
+    std::cout << std::endl << "последовательость X: " << x;
+    std::cout << std::endl << "последовательость Y: " << x;
+    std::cout << std::endl << "                LCS: " << z;
+    std::cout << std::endl << "          длина LCS: " << l;
+    std::cout << std::endl;
+
+    system("pause");
+    return 0;
+}
