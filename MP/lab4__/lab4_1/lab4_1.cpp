acb361719763d25f4f5a093b37f5b3892264f2c3
KuchN <mrkola45@gmail.com>
2025-03-30 23:44:59 +0300

    added MP 4 and 6

---
+++ MP/lab4__/lab4_1/lab4_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <random>
+#include <string>
+
+std::string generate(int length) {
+    std::string nabor = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string result;
+    for (int i = 0; i < length; ++i) {
+        result += nabor[rand() % nabor.size()];
+    }
+
+    return result;
+}
+
+int main() {
+    std::string S1 = generate(300);
+    std::string S2 = generate(200);
+
+    std::cout << "S1: " << S1 << std::endl;
+    std::cout << "S2: " << S2 << std::endl;
+
+    return 0;
+}
--- MP/lab6/lab6.cpp
+++ MP/lab6/lab6.cpp
@@ -1,20 +1,40 @@
-﻿#include "stdafx.h"
+﻿// --- main  
 #include <iostream>
+#include <stack>
 #include "Graph.h"
 #include "BFS.h"
 #include "DFS.h"
 
+
+void topologicalSort(int v, bool visited[], std::stack<int>& Stack, graph::AList& g)
+{
+	
+	visited[v] = true;
+
+	
+	
+	for (int i = 0; i < g.size(v); i++)
+		if (!visited[g.get(v, i)])
+			topologicalSort(g.get(v, i), visited, Stack, g);
+
+	
+	Stack.push(v);
+}
+
 int main()
 {
-	int m[5][5] = {
-				  {0, 1, 0, 1, 1},
-				  {1, 0, 1, 1, 0},
-				  {0, 1, 0, 1, 0},
-				  {1, 1, 1, 0, 1},
-				  {1, 0, 0, 1, 1}
+	int m[7][7] = {
+		//         0  1  2  3  4  5  6
+		/*0*/	  {0, 1, 1, 1, 0, 0, 0},
+		/*1*/     {0, 0, 0, 0, 0, 0, 0},
+		/*2*/	  {0, 0, 0, 0, 0, 0, 0},
+		/*3*/	  {0, 1, 0, 0, 0, 1, 1},
+		/*4*/	  {0, 1, 0, 0, 0, 0, 0},
+		/*5*/	  {0, 0, 1, 0, 0, 0, 0},
+		/*6*/	  {0, 0, 0, 0, 1, 1, 0}
 	};
 	setlocale(LC_ALL, "rus");
-	graph::AMatrix g1(5, (int*)m);
+	graph::AMatrix g1(7, (int*)m);
 	std::cout << std::endl;
 	std::cout << std::endl << "-- матрица смежности " << std::endl;
 	for (int i = 0; i < g1.n_vertex; i++)
@@ -26,7 +46,7 @@ int main()
 
 	graph::AList g2(g1);
 	std::cout << std::endl;
-	std::cout << std::endl << "-- списки смежных вершин " << std::endl;
+	std::cout << std::endl << "-- списки смежных вершин  " << std::endl;
 	for (int i = 0; i < g1.n_vertex; i++)
 	{
 		std::cout << std::endl << i << ": ";
@@ -44,9 +64,9 @@ int main()
 	};
 	std::cout << std::endl;
 
-	graph::AList g4(5, (int*)m);
+	graph::AList g4(7, (int*)m);
 	std::cout << std::endl;
-	std::cout << std::endl << "-- списки смежных вершин " << std::endl;
+	std::cout << std::endl << "-- списки смежных  вершин " << std::endl;
 	for (int i = 0; i < g4.n_vertex; i++)
 	{
 		std::cout << std::endl << i << ": ";
@@ -63,30 +83,43 @@ int main()
 
 	DFS b2(g2);
 	std::cout << std::endl;
-	std::cout << std::endl << "-- поиск в глубину " << std::endl;
-	for (int i = 0; i < g2.n_vertex; i++) std::cout << b2.get(i) << " ";
+	for (int i = g2.n_vertex; i < !0; i--) std::cout << b2.get(i) << " ";
 	std::cout << std::endl;
 
-	int m3[5][5] = {
-					{0, 1, 0, 0, 0},
-					{0, 0, 0, 1, 0},
-					{0, 0, 0, 1, 0},
-					{1, 0, 0, 0, 1},
-					{1, 0, 0, 0, 1}
+	int m3[7][7] = {
+		//         0  1  2  3  4  5  6
+		/*0*/	  {0, 1, 1, 1, 0, 0, 0},
+		/*1*/     {0, 0, 0, 0, 0, 0, 0},
+		/*2*/	  {0, 0, 0, 0, 0, 0, 0},
+		/*3*/	  {0, 1, 0, 0, 0, 1, 1},
+		/*4*/	  {0, 1, 0, 0, 0, 0, 0},
+		/*5*/	  {0, 0, 1, 0, 0, 0, 0},
+		/*6*/	  {0, 0, 0, 0, 1, 1, 0}
 	};
-	graph::AList g5(5, (int*)m3);
+	graph::AList g5(7, (int*)m3);
 	DFS b3(g5);
 	std::cout << std::endl;
-	std::cout << std::endl << "-- поиск в глубину (ориентированный граф) "
+	std::cout << std::endl << "-- поиск в глубину "
 		<< std::endl;
 	for (int i = 0; i < g5.n_vertex; i++) std::cout << b3.get(i) << " ";
 	std::cout << std::endl;
 
-	std::cout << std::endl << "Топологическая сортировка" << std::endl;
-	for (std::vector <int>::iterator i(b3.topological_sort.begin()); i != b3.topological_sort.end(); ++i) std::cout << *i << ' ';
-	std::cout << std::endl;
+	std::cout << std::endl << "-- топологическая сортировка" << std::endl;
+	std::stack<int> Stack;
+	
+	bool* visited = new bool[g5.n_vertex];
+	for (int i = 0; i < g5.n_vertex; i++) visited[i] = false;
+	
+	for (int i = 0; i < g5.n_vertex; i++)
+		if (visited[i] == false)
+			topologicalSort(i, visited, Stack, g5);
 
+	while (Stack.empty() == false)
+	{
+		std::cout << Stack.top() << " ";
+		Stack.pop();
+	}
+	std::cout << std::endl;
 	system("pause");
 	return 0;
 }
-
